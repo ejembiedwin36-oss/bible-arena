5c566a82c4a3e24fcc5a554b51f22cf4b1de73fb
ejembiedwin36-oss <ejembiedwin36@gmail.com>
2026-09-14 14:25:08 +0100

    feat: initialize Bible Arena frontend foundation

---
+++ src/main.tsx
@@ -0,0 +1,1 @@
+import { StrictMode } from 'react';\nimport { createRoot } from 'react-dom/client';\nimport { BrowserRouter } from 'react-router-dom';\nimport App from './App';\nimport './styles.css';\n\ncreateRoot(document.getElementById('root')!).render(\n  <StrictMode>\n    <BrowserRouter>\n      <App />\n    </BrowserRouter>\n  </StrictMode>,\n);\n
